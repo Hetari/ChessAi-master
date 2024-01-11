f9baad3a0ad8f857e1ce15a1436736f472a12b07
Hetari <hetari4py@gmail.com>
2024-01-11 19:59:34 +0300

    add the draw_board

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -1,8 +1,8 @@
+import ChessEngine
 import pygame as p
-from Chess import ChessEngine
+import numpy as np
 
-
-WIDTH = HEIGHT = 512  # 400 is another option
+WIDTH = HEIGHT = 800  # 400 is another option
 DIMENSION = 8  # dimension is 8*8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15  # for animation
@@ -28,4 +28,103 @@ def load_images() -> None:
     # note: we can access an image by saying "IMAGES[1]"
     for piece in range(1, 13):
         IMAGES[piece] = p.transform.scale(p.image.load(
-            f'images/{piece}.png'), (SQ_SIZE, SQ_SIZE))
+            f'./Chess/images/{piece}.png'), (SQ_SIZE, SQ_SIZE))
+
+
+def draw_game_state(screen: p.Surface, game_state: ChessEngine.GameState) -> None:
+    """
+    Draw the current game state on the screen. This includes the board and the pieces.
+
+    Args:
+        screen (p.Surface): The surface object representing the screen to draw on.
+        game_state (ChessEngine.GameState): The current game state.
+
+    Returns:
+        None
+    """
+    draw_board(screen)  # draw squares on the board
+    # draw pieces on top of those squares
+    draw_pieces(screen, game_state.board)
+
+    # TODO: draw move log
+    # TODO: draw piece highlighting
+
+
+def draw_board(screen: p.Surface) -> None:
+    """
+    Draw the squares on the board.
+
+    Args:
+        screen (p.Surface): The surface object representing the screen to draw on.
+
+    Returns:
+        None
+    """
+    colors = [p.Color("white"), p.Color("gray")]
+
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            # Determine the color of the square based on its position where (col + row) % 2) is 0 or 1, in other words:
+            # if we are on (0, 0) -> 0 + 0 = 0 -> 0 % 2 = 0 (even number so the color is white)
+            # or if we are on (0, 1) so 0 + 1 = 1 and 1 % 2 = 1 (odd number so the color is gray)
+            color = colors[((col + row) % 2)]
+
+            # Draw a rectangle representing the square on the screen
+            p.draw.rect(
+                screen,  # The surface object representing the screen to draw on
+                color,   # The color of the rectangle
+                p.Rect(
+                    col * SQ_SIZE,   # The x-coordinate of the top-left corner of the rectangle
+                    row * SQ_SIZE,   # The y-coordinate of the top-left corner of the rectangle
+                    SQ_SIZE,         # The width of the rectangle
+                    SQ_SIZE          # The height of the rectangle
+                )
+            )
+
+
+def draw_pieces(screen: p.Surface, board: np.ndarray) -> None:
+    pass
+
+
+def main() -> None:
+    """
+    ### Main function
+
+    This function initializes Pygame and sets up the game loop. The game loop
+    consists of the following steps:
+
+    1. Initialize the game state
+    2. Handle events
+    3. Update the game state
+    4. Draw the game state
+    5. Update the display
+    6. Limit the game speed
+
+    Parameters:
+        `None`
+
+    Returns:
+        `None`
+    """
+
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+
+    game_state = ChessEngine.GameState()
+
+    load_images()  # do this only once, before the while loop
+    running = True
+    while running:
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                running = False
+
+        draw_game_state(screen, game_state)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+if __name__ == "__main__":
+    main()
