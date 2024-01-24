a19d83faf81afc63bfe374e2f5be6395a8fa1995
Hetari <hetari4py@gmail.com>
2024-01-25 02:25:16 +0300

    Refactor ChessEngine.py to use type hints and docstring

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -4,6 +4,23 @@ import pygame as p
 
 
 class GameState():
+    """
+    The GameState class keeps track of the chess board, the current player's turn, and the move history. It provides methods to make moves, undo moves, and get all possible moves for the current state of the game.
+
+    Attributes:
+        board: A 2D list representing the chess board.
+        move_functions: A dictionary mapping piece types to their corresponding move functions.
+        white_to_move: A boolean indicating whether it is currently white's turn to move.
+        move_log: A list of Move objects representing the move history.
+
+    Methods:
+        make_move: Makes a move on the chess board.
+        undo_move: Undoes the last move made in the game.
+        get_valid_moves: Returns a list of all valid moves for the current state of the game.
+        get_all_possible_moves: Returns a list of all possible moves for the current state of the game.
+        get_pawn_moves: Generates valid moves for a pawn at a given position.
+    """
+
     def __init__(self) -> None:
         # board is 8x8 2d list, each element of list has 2 characters
         # 0 represents an empty space
@@ -19,7 +36,7 @@ class GameState():
         # 10 represents a white queen
         # 11 represents a white king
         # 12 represents a white pawn
-        self.board = np.array([
+        self.board: np.ndarray = np.array([
             [1, 2, 3, 4, 5, 3, 2, 1],
             [6, 6, 6, 6, 6, 6, 6, 6],
             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -31,15 +48,26 @@ class GameState():
         ], dtype=np.int8)
 
         # Get the possible moves for each piece
-        self.move_functions = {
+        self.move_functions: dict[int, callable] = {
             6: self.get_pawn_moves,
             12: self.get_pawn_moves
         }
 
-        self.white_to_move = True
-        self.move_log = []
+        self.white_to_move: bool = True
+        self.move_log: list[Move.Move] = []
 
     def make_move(self, move: Move) -> None:
+        """
+        Makes a move on the chess board.
+
+        This method updates the chess board, the current player's turn, and the move history based on the given move.
+
+        Args:
+            move (Move): The move to be made.
+
+        Returns:
+            None
+        """
         # Make the piece location empty
         self.board[move.start_row][move.start_col] = 0
 
@@ -54,11 +82,10 @@ class GameState():
 
     def undo_move(self):
         """
-        Undo the last move made in the game.
+        Undoes the last move made in the game.
+        This method removes the last move from the move log and updates the board accordingly. If there are no moves in the move log, the method returns without making any changes to the game state.
 
-        This function removes the last move from the move log and updates the board accordingly. If there are no moves in the move log, the function returns without making any changes to the game state.
-
-        Parameters:
+        Args:
             None
 
         Returns:
@@ -66,22 +93,33 @@ class GameState():
         """
         if len(self.move_log) == 0:
             return
-        move = self.move_log.pop()
+        move: Move.Move = self.move_log.pop()
         self.board[move.start_row][move.start_col] = move.piece_moved
         self.board[move.end_row][move.end_col] = move.piece_captured
         self.white_to_move = not self.white_to_move
 
-    def get_valid_moves(self):
+    def get_valid_moves(self) -> list[Move.Move]:
+        """
+        Returns a list of all valid moves for the current state of the game.
+
+        This method calls the get_all_possible_moves method to get all possible moves and filters out the invalid moves.
+
+        Args:
+            None
+
+        Returns:
+            list[Move]: A list of all valid moves.
+        """
         return self.get_all_possible_moves()
 
-    def get_all_possible_moves(self):
+    def get_all_possible_moves(self) -> list[Move.Move]:
         """
         Get all possible and valid moves for the current state of the game.
 
         Returns:
             list[Move]: A list of all possible and valid moves.
         """
-        moves = []
+        moves: list[Move.Move] = []
         for row in range(len(self.board)):
             for col in range(len(self.board[row])):
                 piece = self.board[row][col]
@@ -174,40 +212,105 @@ class GameState():
 
 
 class Color:
-    def __init__(self, light, dark):
-        self.light = light
-        self.dark = dark
+    """
+    class Color:
+    Represents a color with a light and dark shade.
+
+    Args:
+        light: The light shade of the color.
+        dark: The dark shade of the color.
+    """
+
+    def __init__(self, light: tuple[int, int, int], dark: tuple[int, int, int]):
+        self.light: tuple[int, int, int] = light
+        self.dark: tuple[int, int, int] = dark
 
 
 class Theme:
-    def __init__(self, light_bg, dark_bg,
-                 light_trace, dark_trace,
-                 light_moves, dark_moves):
-        self.bg = Color(light_bg, dark_bg)
-        self.trace = Color(light_trace, dark_trace)
-        self.moves = Color(light_moves, dark_moves)
+    """
+    Represents a theme for a chess game.
+
+    Args:
+        light_bg: The light background color.
+        dark_bg: The dark background color.
+        light_trace: The light color for trace lines.
+        dark_trace: The dark color for trace lines.
+        light_moves: The light color for move indicators.
+        dark_moves: The dark color for move indicators.
+    """
+
+    def __init__(self, light_bg: int, dark_bg: int,
+                 light_trace: int, dark_trace: int,
+                 light_moves: int, dark_moves: int):
+        self.bg: Color = Color(light_bg, dark_bg)
+        self.trace: Color = Color(light_trace, dark_trace)
+        self.moves: Color = Color(light_moves, dark_moves)
 
 
 class Config():
-    def __init__(self) -> None:
-        self.themes = []
+    """
+    Represents the configuration for a chess game.
+
+    Attributes:
+        themes: A list of available themes.
+        idx: The index of the current theme.
+        theme: The current theme.
+
+    Methods:
+        change_theme: Changes the current theme to the next one.
+    """
+
+    def __init__(self):
+        self.themes: list[Theme] = []
         self._add_themes()
-        self.idx = 0
-        self.theme = self.themes[self.idx]
+        self.idx: int = 0
+        self.theme: Theme = self.themes[self.idx]
 
     def change_theme(self):
+        """
+        Changes the current theme to the next one.
+        """
         self.idx += 1
         self.idx %= len(self.themes)
         self.theme = self.themes[self.idx]
 
     def _add_themes(self):
-        green = Theme((234, 235, 200), (119, 154, 88), (244, 247,
-                      116), (172, 195, 51), '#C86464', '#C84646')
-        brown = Theme((235, 209, 166), (165, 117, 80), (245, 234,
-                      100), (209, 185, 59), '#C86464', '#C84646')
-        blue = Theme((229, 228, 200), (60, 95, 135), (123, 187,
-                     227), (43, 119, 191), '#C86464', '#C84646')
-        gray = Theme((120, 119, 118), (86, 85, 84), (99, 126, 143),
-                     (82, 102, 128), '#C86464', '#C84646')
+        """
+        Adds predefined themes to the list of available themes.
+        """
+        green: Theme = Theme(
+            (234, 235, 200),
+            (119, 154, 88),
+            (244, 247, 116),
+            (172, 195, 51),
+            '#C86464',
+            '#C84646',
+        )
+
+        brown: Theme = Theme(
+            (235, 209, 166),
+            (165, 117, 80),
+            (245, 234, 100),
+            (209, 185, 59),
+            '#C86464',
+            '#C84646',
+        )
+        blue: Theme = Theme(
+            (229, 228, 200),
+            (60, 95, 135),
+            (123, 187, 227),
+            (43, 119, 191),
+            '#C86464',
+            '#C84646',
+        )
+
+        gray: Theme = Theme(
+            (120, 119, 118),
+            (86, 85, 84),
+            (99, 126, 143),
+            (82, 102, 128),
+            '#C86464',
+            '#C84646',
+        )
 
         self.themes = [green, brown, blue, gray]
