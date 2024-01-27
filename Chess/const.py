f71a815c706ba00250cc1524af5e267020f15d8f
Hetari <hetari4py@gmail.com>
2024-01-27 22:10:19 +0300

    Make the screen size bigger

--- Chess/const.py
+++ Chess/const.py
@@ -1,7 +1,7 @@
 from ChessEngine import Config
 
 
-WIDTH = HEIGHT = 512  # 400 is another option
+WIDTH = HEIGHT = 800  # 400 is another option
 ROWS = COLS = 8  # dimension is 8*8
 SQ_SIZE = HEIGHT // COLS
 MAX_FPS = 15  # for animation
