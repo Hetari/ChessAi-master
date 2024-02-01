cce588560b98416c0a89f7f51f729a5fd0e1ee83
Hetari <hetari4py@gmail.com>
2024-02-02 01:54:42 +0300

    fix screen size of the game and make it dynamic

--- Chess/src/const.py
+++ Chess/src/const.py
@@ -1,10 +1,25 @@
+from math import ceil
 from src.Config import Config
+from screeninfo import get_monitors
 
 
-WIDTH = HEIGHT = 800  # 400 is another option
+monitors = get_monitors()
+width = 0
+height = 0
+for monitor in monitors:
+    if monitor.is_primary:
+        width = monitor.width
+        height = monitor.height
+        break
+
+if width == 0:
+    width = height = 1000
+
+HEIGHT = WIDTH = int(height * 0.75) - int(height * 0.75) % 100
+
 ROWS = COLS = 8  # dimension is 8*8
-SQ_SIZE = HEIGHT // COLS
-MAX_FPS = 10  # for animation
+SQ_SIZE = ceil(HEIGHT / COLS)
+MAX_FPS = 15  # for animation
 IMAGES = {}
 
 config = Config()
